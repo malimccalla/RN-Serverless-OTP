840e9dcacdb2fc11ca6e0a3059228cf18d0be417
Mali Michael <malimccalla@gmail.com>
2017-07-26 22:56:47 +0100

    Adds create user function set up

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const createUser = require('./create-user');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.goodbye = functions.https.onRequest((req, res) => {
+  res.json({ message: 'Goodbye!' });
+});
+
+exports.createUser = functions.https.onRequest(createUser);
