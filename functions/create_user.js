dce9658d69dc1855b759afdf8b28260d39dddbac
Mali Michael <malimccalla@gmail.com>
2017-07-27 12:05:54 +0100

    Updates create user lambda

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,13 @@
+const admin = require('firebase-admin');
+
 module.exports = (req, res) => {
-  res.json(req.body);
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Phone number required'});
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+  admin.auth().createUser({ uid: phone })
+  .then((user) => res.send(user))
+  .catch(err => res.status(422).send({ error: err }));
 }
