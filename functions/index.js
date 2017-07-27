7d045b9dab2479cb22f3234ade2976b9e98f428a
Mali Michael <malimccalla@gmail.com>
2017-07-27 09:12:09 +0100

    Set up service account

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,11 @@
 const functions = require('firebase-functions');
-const createUser = require('./create-user');
+const admin = require('firebase-admin');
+const createUser = require('./create_user');
+const serviceAccount = require('./service_account.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.goodbye = functions.https.onRequest((req, res) => {
-  res.json({ message: 'Goodbye!' });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://one-time-password-60645.firebaseio.com'
 });
 
 exports.createUser = functions.https.onRequest(createUser);
