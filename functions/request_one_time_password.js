45fcd712546b081ebe3d076c9e3bff9e862e9c27
Mali Michael <malimccalla@gmail.com>
2017-07-28 14:47:39 +0100

    Verify one time password

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -13,11 +13,18 @@ module.exports = (req, res) => {
       const code = Math.floor(Math.random() * 8999 + 1000);
 
       twilio.messages.create({
-        body: `You Tunebay verification code is ${code}.`,
+        body: `Your security code is ${code}`,
         to: phone,
         from: '+442033224658'
-      })
+      }, (err) => {
+        if (err) { return res.status(422).send({ err: error }); }
 
+        admin.database().ref(`users/${phone}`)
+          .update({ code, codeValid: true }, () => {
+            res.send({ success: true });
+          });
+
+      })
     })
     .catch((err) => {
       res.status(422).json({ error: err });
